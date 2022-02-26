8586a10fee110ac8e95cf5328349664929d2ea18
Tomas Licciardi Lopez <tomilrclicciardi@gmail.com>
2022-02-26 16:52:34 -0300

    SegundaEntrega JS

--- js/primeraEntrega.js
+++ js/primeraEntrega.js
@@ -1,84 +1,84 @@
-alert("Bienvenido a nuestra muebleria!!");
+// alert("Bienvenido a nuestra muebleria!!");
 
-let muebles = parseInt(prompt("1: sillas 2: mesas 3: escritorio 4: armario"));
-let eleccion = "";
-let material = "";
+// let muebles = parseInt(prompt("1: sillas 2: mesas 3: escritorio 4: armario"));
+// let eleccion = "";
+// let material = "";
 
-while (muebles == 1 || muebles == 2 || muebles == 3 || muebles == 4) {
-  if (muebles == 1) {
-    eleccion = "sillas";
-    alert(`Usted ha elegido ${eleccion}`);
-    material = prompt("¿De que material desea la silla?");
-    if (
-      material.toLocaleLowerCase() == "nogal" ||
-      material.toLocaleLowerCase() == "roble"
-    ) {
-      alert(
-        `Perfecto! El material con que le daremos la silla es de ${material}`
-      );
-      break;
-    } else {
-      alert("Lo siento! Solo puedes escoger entre nogal o roble");
-      break;
-    }
-  } else if (muebles == 2) {
-    eleccion = "mesas";
-    alert(`Usted ha elegido ${eleccion}`);
-    material = prompt("¿De que material desea la mesa?");
-    if (
-      material.toLocaleLowerCase() == "pino" ||
-      material.toLocaleLowerCase() == "haya" ||
-      material.toLocaleLowerCase() == "nogal"
-    ) {
-      alert(
-        `Perfecto! El material con que le daremos la mesa es de ${material}`
-      );
-      break;
-    } else {
-      alert("Lo siento! Solo puedes escoger entre nogal, pino o haya");
-      break;
-    }
-  } else if (muebles == 3) {
-    eleccion = "escritorio";
-    alert(`Usted ha elegido ${eleccion}`);
-    material = prompt("¿De que material desea el escritorio?");
-    if (
-      material.toLocaleLowerCase() == "fresno" ||
-      material.toLocaleLowerCase() == "castaño" ||
-      material.toLocaleLowerCase() == "roble"
-    ) {
-      alert(
-        `Perfecto! El material con que le daremos la mesa es de ${material}`
-      );
-      break;
-    } else {
-      alert("Lo siento! Solo puedes escoger entre fresno, castaño o roble");
-      break;
-    }
-  } else if (muebles == 4) {
-    eleccion = "armario";
-    alert(`Usted ha elegido ${eleccion}`);
-    material = prompt("¿De que material desea el armario?");
-    if (
-      material.toLocaleLowerCase() == "nogal" ||
-      material.toLocaleLowerCase() == "pino" ||
-      material.toLocaleLowerCase() == "castaño"
-    ) {
-      alert(
-        `Perfecto! El material con que le daremos el armario es de ${material}`
-      );
-      break;
-    } else {
-      alert("Lo siento! Solo puedes escoger entre nogal, castaño o pino");
-      break;
-    }
-  }
-}
-if (muebles < 1 || muebles > 4) {
-  alert("No contamos con lo que usted desea! Lo sentimos!");
-}
+// while (muebles == 1 || muebles == 2 || muebles == 3 || muebles == 4) {
+//   if (muebles == 1) {
+//     eleccion = "sillas";
+//     alert(`Usted ha elegido ${eleccion}`);
+//     material = prompt("¿De que material desea la silla?");
+//     if (
+//       material.toLocaleLowerCase() == "nogal" ||
+//       material.toLocaleLowerCase() == "roble"
+//     ) {
+//       alert(
+//         `Perfecto! El material con que le daremos la silla es de ${material}`
+//       );
+//       break;
+//     } else {
+//       alert("Lo siento! Solo puedes escoger entre nogal o roble");
+//       break;
+//     }
+//   } else if (muebles == 2) {
+//     eleccion = "mesas";
+//     alert(`Usted ha elegido ${eleccion}`);
+//     material = prompt("¿De que material desea la mesa?");
+//     if (
+//       material.toLocaleLowerCase() == "pino" ||
+//       material.toLocaleLowerCase() == "haya" ||
+//       material.toLocaleLowerCase() == "nogal"
+//     ) {
+//       alert(
+//         `Perfecto! El material con que le daremos la mesa es de ${material}`
+//       );
+//       break;
+//     } else {
+//       alert("Lo siento! Solo puedes escoger entre nogal, pino o haya");
+//       break;
+//     }
+//   } else if (muebles == 3) {
+//     eleccion = "escritorio";
+//     alert(`Usted ha elegido ${eleccion}`);
+//     material = prompt("¿De que material desea el escritorio?");
+//     if (
+//       material.toLocaleLowerCase() == "fresno" ||
+//       material.toLocaleLowerCase() == "castaño" ||
+//       material.toLocaleLowerCase() == "roble"
+//     ) {
+//       alert(
+//         `Perfecto! El material con que le daremos la mesa es de ${material}`
+//       );
+//       break;
+//     } else {
+//       alert("Lo siento! Solo puedes escoger entre fresno, castaño o roble");
+//       break;
+//     }
+//   } else if (muebles == 4) {
+//     eleccion = "armario";
+//     alert(`Usted ha elegido ${eleccion}`);
+//     material = prompt("¿De que material desea el armario?");
+//     if (
+//       material.toLocaleLowerCase() == "nogal" ||
+//       material.toLocaleLowerCase() == "pino" ||
+//       material.toLocaleLowerCase() == "castaño"
+//     ) {
+//       alert(
+//         `Perfecto! El material con que le daremos el armario es de ${material}`
+//       );
+//       break;
+//     } else {
+//       alert("Lo siento! Solo puedes escoger entre nogal, castaño o pino");
+//       break;
+//     }
+//   }
+// }
+// if (muebles < 1 || muebles > 4) {
+//   alert("No contamos con lo que usted desea! Lo sentimos!");
+// }
 
-alert("Saludos y muchas gracias por visitarnos!");
+// alert("Saludos y muchas gracias por visitarnos!");
 
 // if (muebles === 1) {
 //   eleccion = "sillas";
---
+++ js/segundaEntrega.js
@@ -0,0 +1,69 @@
+alert("Bienvenido a la calculadora mas inteligente del mundo! Saludos");
+
+alert("Escribir SALIR " + "para finalizar la calculadora");
+
+let esc = prompt("¿Que operacion desea realizar?");
+
+function suma(num1, num2) {
+  resultadoSuma = num1 + num2;
+  return resultadoSuma;
+}
+
+function resta(num1, num2) {
+  resultadoResta = num1 - num2;
+  return resultadoResta;
+}
+
+function multiplicar(num1, num2) {
+  resultadoMultiplicar = num1 * num2;
+  return resultadoMultiplicar;
+}
+
+function dividir(num1, num2) {
+  resultadoDividir = num1 / num2;
+  return resultadoDividir;
+}
+
+let primerNumero;
+let segundoNumero;
+
+while (esc.toLowerCase() != "salir") {
+  switch (esc.toLowerCase()) {
+    case "suma":
+      primerNumero = parseInt(prompt("Primer numero para la operacion"));
+      segundoNumero = parseInt(prompt("Segundo numero para la operacion"));
+      suma(primerNumero, segundoNumero);
+      alert(resultadoSuma);
+      esc = prompt("¿Que operacion desea realizar?");
+      break;
+    case "resta":
+      primerNumero = parseInt(prompt("Primer numero para la operacion"));
+      segundoNumero = parseInt(prompt("Segundo numero para la operacion"));
+      resta(primerNumero, segundoNumero);
+      alert(resultadoResta);
+      esc = prompt("¿Que operacion desea realizar?");
+
+      break;
+    case "dividir":
+      primerNumero = parseInt(prompt("Primer numero para la operacion"));
+      segundoNumero = parseInt(prompt("Segundo numero para la operacion"));
+      dividir(primerNumero, segundoNumero);
+      alert(resultadoDividir);
+      esc = prompt("¿Que operacion desea realizar?");
+
+      break;
+    case "multiplicar":
+      primerNumero = parseInt(prompt("Primer numero para la operacion"));
+      segundoNumero = parseInt(prompt("Segundo numero para la operacion"));
+      multiplicar(primerNumero, segundoNumero);
+      alert(resultadoMultiplicar);
+      esc = prompt("¿Que operacion desea realizar?");
+
+      break;
+    default:
+      alert("No puso ninguna operacion");
+      esc = prompt("¿Que operacion desea realizar?");
+      break;
+  }
+}
+alert("Gracias por utilizar la mejor calculadora de la historia!");
